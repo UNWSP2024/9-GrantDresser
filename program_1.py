e30da366dadb7967cda7784716053d20900d3792
GrantDresser <gfdresser@students.unwsp.edu>
2025-10-31 20:16:57 -0500

    Week 9 Programs

--- program_1.py
+++ program_1.py
@@ -4,12 +4,13 @@
 # Write a program that displays the number of names that are stored in the file.
 
 def count_file_lines():
-    ######################
-    # Add your code here #
-    ######################
-    print('In the count_file_lines function')
-
-
+    try:
+        with open('names.txt', 'r') as file:
+            lines = [line.strip() for line in file if line.strip() != '']
+            count = len(lines)
+            print (f'There are {count} names in the file.')
+    except FileNotFoundError:
+        print ('Error: The file names.txt was not found.')
 
 # You don't need to change anything below this line:
 if __name__ == '__main__':
